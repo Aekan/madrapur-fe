24a412d86ccf21606af5d0feec7e9b2d5a09c429
Aekan <alpe15.1992@gmail.com>
2019-04-25 05:30:57 +0200

    * introduced axios
    * fixed history warning
    * fixed nested a warning
    * added test API call

--- src/js/common/components/Header/Header.jsx
+++ src/js/common/components/Header/Header.jsx
@@ -38,10 +38,10 @@ class Header extends PureComponent {
         <div className="container">
           <div className="row">
             <nav className="navbar navbar-expand-lg navbar-light">
-              <a className="navbar-brand" href="#">
+              <span className="navbar-brand" href="#">
                 {' '}
                 <Link to="/"><img alt="logo" src="https://silver-line.hu/wp-content/uploads/2017/05/budapest_river_cruise_logo.png" /></Link>
-              </a>
+              </span>
               <button className="navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon" />
               </button>
--- src/js/redux/sagas/shopSaga.js
+++ src/js/redux/sagas/shopSaga.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { put, fork, takeLatest } from 'redux-saga/effects';
 import { constants as shopConstants, actions as shopActions } from '../modules/shop';
 
@@ -5,6 +6,23 @@ import type { shopType } from '../../common/types/shop';
 
 export function* fetchShopData() {
   // pretend there is an api call
+
+  axios
+    .get('http://api.madrapur.lh/v1/rester/product', {
+      params: {
+        ID: 2,
+      },
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .then(() => {
+      // always executed
+    });
+
   const result: shopType = {
     items: [],
     addedItems: [],
